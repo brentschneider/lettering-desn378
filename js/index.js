39603362ad64cb48b369bef46b1f68bf8c1d664a
brentschneider <brent@milkshakeinteractive.com>
2015-10-08 13:12:01 -0700

    added lettering plugin to jquery

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // My comments
+
+
+// Get & Store First name
+     //var firstName = prompt();
+// Get & Store Last name
+     //var lastName = prompt();
+// Display First & Last name
+
+// $('.firstName').text(firstName);
+// $('.lastName').text(lastName);
+
+
+// turn on lettering
+$(document).ready(function() {
+    $(".fancy_title").lettering();
+});
